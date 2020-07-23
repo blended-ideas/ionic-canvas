8c71dbc2850180b0d5da31f1a7fb8eb422f6ce74
Karthik RP <karthik.rp@hotmail.com>
2020-07-23 23:50:45 +0530

    Working Canvas draw and get canvas to base64.

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,18 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {fromEvent} from 'rxjs';
+import {pairwise, switchMap, takeUntil} from 'rxjs/operators';
+import {Platform} from '@ionic/angular';
+
+interface TouchPosition {
+    x: number;
+    y: number;
+}
 
 @Component({
-  selector: 'app-folder',
-  templateUrl: './folder.page.html',
-  styleUrls: ['./folder.page.scss'],
+    selector: 'app-folder',
+    templateUrl: './folder.page.html',
+    styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
-  public folder: string;
+export class FolderPage implements AfterViewInit {
+    @ViewChild('signatureCanvas') public signatureCanvas: ElementRef<HTMLCanvasElement>;
+    private cx: CanvasRenderingContext2D;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+    constructor(private platform: Platform) {
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id');
-  }
+    }
 
+    public ngAfterViewInit() {
+        const canvasEl: HTMLCanvasElement = this.signatureCanvas.nativeElement;
+        this.cx = canvasEl.getContext('2d');
+
+        console.log(this.platform.width(), this.platform.height());
+        canvasEl.width = this.platform.width();
+        canvasEl.height = this.platform.height() - 44;
+
+        this.cx.lineWidth = 3;
+        this.cx.lineCap = 'round';
+        this.cx.strokeStyle = '#000000';
+
+        this.captureEvents(canvasEl);
+    }
+
+    saveImage() {
+        const dataURL = this.signatureCanvas.nativeElement.toDataURL();
+        console.log(dataURL);
+    }
+
+    private captureEvents(canvasEl: HTMLCanvasElement) {
+        fromEvent(canvasEl, 'touchstart')
+            .pipe(
+                switchMap((e) => fromEvent(canvasEl, 'touchmove')
+                    .pipe(
+                        takeUntil(fromEvent(canvasEl, 'touchleave')),
+                        pairwise() /* Return the previous and last values as array */
+                    )
+                )
+            ).subscribe((touchEvents: [TouchEvent, TouchEvent]) => {
+            const rect = canvasEl.getBoundingClientRect();
+            const touchPositions: TouchPosition[] = touchEvents.map(te => ({
+                x: te.touches[0].clientX - rect.left,
+                y: te.touches[0].clientY - rect.top
+            }));
+            this.drawOnCanvas(touchPositions[0], touchPositions[1]);
+        });
+    }
+
+    private drawOnCanvas(prevPos: TouchPosition, currentPos: TouchPosition) {
+        if (!this.cx) {
+            return;
+        }
+
+        this.cx.beginPath();
+
+        if (prevPos) {
+            this.cx.moveTo(prevPos.x, prevPos.y); // from
+            this.cx.lineTo(currentPos.x, currentPos.y);
+            this.cx.stroke();
+        }
+    }
 }
